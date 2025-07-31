d47600f553e4cdabc9dd71bb92c620604b8be2b5
Yixun-Hu <yixunhu21@gmail.com>
2025-07-31 06:15:33 +0000

    Deploying to gh-pages from @ Yixun-Hu/Yixun-Hu.github.io@bac43da9cb0813955bcab30b7c6eba0af58d39a6 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -440,8 +440,8 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
+            },},{id: "news-paper-pspc-is-accepted-to-ojid-2024",
+          title: 'Paper PSPC is accepted to OJID 2024.',
           description: "",
           section: "News",},{id: "news-a-long-announcement-with-details",
           title: 'A long announcement with details',
